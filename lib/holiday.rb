74107e7bf718e1c31ae81d6e0e47ef1eff077ce0
Joel S <joel29dec@gmail.com>
2019-04-09 18:20:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,26 +18,54 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  activities = []
+  holiday_hash.each do |seasons, holiday|
+    if seasons == :summer
+      holiday.each do |season, activity|
+        activities << activity
+      end
+    end
+  end
+  activities[0][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_hash.each do |seasons, holiday|
+    if seasons == :winter
+      holiday.each do |season, activity|
+        activity << supply
+      end
+      puts holiday_hash
+    end
+  end
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash.each do |seasons, holiday|
+  if seasons == :spring
+      holiday.each do |season, activity|
+        activity << supply
+      end
+      puts holiday_hash
+    end
+  end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  holiday_hash.each do |seasons, holiday|
+    if seasons == season
+      season.merge!({:holiday_name => supply_array}) 
+      end
+    end
+    holiday_hash
 end
 
 def all_winter_holiday_supplies(holiday_hash)
@@ -53,8 +81,17 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |seasons, holiday|
+    puts "#{seasons.capitalize}:"
+    holiday.each do |season, activity|
+      capital = season.to_s.split("_")
+      capital.each {|i| i.capitalize!}
+      capital = capital.join(" ")
+    puts "  #{capital}: #{activity.join(", ")}"
+    end
+  end
 end
+
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
